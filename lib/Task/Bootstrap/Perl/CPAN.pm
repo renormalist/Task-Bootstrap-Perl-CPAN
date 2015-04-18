cee73bfaf09a2e65726fc52a61982b1f6fa5ad04
Steffen Schwigon <ss5@renormalist.net>
2015-04-18 19:05:42 +0200

    Dependencies for App::Bootstrap::Perl - CPAN bundle

---
+++ lib/Task/Bootstrap/Perl/CPAN.pm
@@ -0,0 +1,71 @@
+use strict;
+use warnings;
+package Task::Bootstrap::Perl::CPAN;
+# ABSTRACT: Dependencies for App::Bootstrap::Perl - CPAN bundle
+
+
+=pkgroup cpan
+
+=pkg Test::Simple
+
+=pkg List::MoreUtils
+
+=pkg version
+
+=pkg YAML::Syck
+
+=pkg IO::Tty
+
+=pkg LWP
+
+=pkg Expect
+
+=pkg ExtUtils::Config
+
+=pkg ExtUtils::MakeMaker
+
+=pkg Test::Harness
+
+=pkg ExtUtils::CBuilder
+
+=pkg File::Temp
+
+=pkg Test::More
+
+=pkg Data::Dumper
+
+=pkg IO::Compress::Base
+
+=pkg Compress::Zlib
+
+=pkg IO::Zlib
+
+=pkg Archive::Tar
+
+=pkg Module::Build
+
+=pkg File::Spec
+
+=pkg Digest::SHA
+
+=pkg File::HomeDir
+
+=pkg Archive::Zip
+
+=pkg Net::FTP
+
+=pkg Term::ReadKey
+
+=pkg Term::ReadLine::Perl
+
+=pkg YAML
+
+=pkg Parse::CPAN::Meta
+
+=pkg Text::Glob
+
+=pkg CPAN
+
+=pkg File::Which
+
+1;
